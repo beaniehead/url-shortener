509b51848dec188ceaff85fda213bdee1099a668
Glitch (tabby-belly) <none>
2018-01-20 05:42:22 +0000

    ✋😳 Checkpoint controllers/controller.js:576250/857 views/index.html:576250/6

--- controllers/controller.js
+++ controllers/controller.js
@@ -28,9 +28,24 @@ exports.urlshorten = (req, res) => {
       }
     }).toArray((err, docs) => {
       if (err) throw err;
-      const id = (docs[0]["_id"]).toString();
-      if (docs === null) {
-        //create matching pair and add to db
+      //if the document does exist return the object to the user
+      
+      //if the document doesn't exist - 
+      
+      // 1. create a new document and add to db with the entered url as the olrUrl
+      // 2. create a unique suffix based on the new document id
+      // 3. add newUrl to the above document.
+      
+      // or 
+      
+      
+      // 1. generate a suffix for new url
+      // 2. creat object with new url and old url
+      // 3. add this to db as new databse
+      
+      //option 2 is simpler - just need to generate the suffix
+        //create unique has suffix based on the 
+        const id = (docs[0]["_id"]).toString();
         function createHash(id) {
           const hashids = new Hashids();
           return hashids.encodeHex(id);
@@ -38,9 +53,9 @@ exports.urlshorten = (req, res) => {
         const base = "https://shorts.glitch.me/"
          const suffix = createHash(id);
         console.log(suffix)
-      } else {
+       
         // return old and new url as json object to user
-      }
+      
     });
   });
   res.send(res.locals.valid);
