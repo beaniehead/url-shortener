a3cf9412c74de6b49c197f2d47f00fd8c036b9f5
Glitch (shorts) <none>
2018-01-20 20:25:17 +0000

    😬🎊 Checkpoint controllers/controller.js:576250/249

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,9 +17,10 @@ exports.urlvalidate = (req, res, next) => {
 exports.urlshorten = (req, res) => {
   let exists = false;
   const url = process.env.DATABASE;
+  // Connect to DB
   MongoClient.connect(url, (err, db) => {
     assert.equal(null, err);
-    if (err) log err;
+    if (err) throw err;
     console.log("Successfully connected to db");
     const dbo = db.db("url-shortener");
     //logic - check to see if entered url exists in database
@@ -31,28 +32,9 @@ exports.urlshorten = (req, res) => {
     }).toArray((err, docs) => {
       if (err) throw err;
       //if the document does exist return the object to the user
+      //function getData(){}
       exists = true;
       //if the document doesn't exist - 
-      
-       
-      
-      // 1. generate a suffix for new url (using a hash of the timestamp)
-      // 2. creat object with new url and old url
-      // 3. add this to db as new databse
-      
-      //option 2 is simpler - just need to generate the suffix
-        //create unique has suffix based on the 
-        const id = (docs[0]["_id"]).toString();
-        function createHash(id) {
-          const hashids = new Hashids();
-          return hashids.encodeHex(id);
-        };
-        const base = "https://shorts.glitch.me/"
-         const suffix = createHash(id);
-        console.log(suffix)
-       
-        // return old and new url as json object to user
-      
     });
   });
   res.send(res.locals.valid);
