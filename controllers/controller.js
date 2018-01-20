8681e51348a04c33977f12f5a26908f50b630c96
Glitch (shorts) <none>
2018-01-20 03:09:45 +0000

    🍗🌚 Checkpoint controllers/controller.js:576250/7 server.js:576250/4

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const validateURL = require("valid-url");
 const url = require("url");
+const mongoose = require('mongoose');
 exports.urlvalidate = (req, res, next) => {
   const urii = req.params[0];
   const valid = validateURL.isHttpsUri(urii);
@@ -11,6 +12,13 @@ exports.urlvalidate = (req, res, next) => {
   };
 };
 exports.urlshorten = (req, res) => {
-  console.log("Next");
+  // Connect to our Database and handle any bad connections
+  mongoose.connect(process.env.DATABASE, (err) => {
+    if (!err) console.log("connected");
+  });
+  mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+  mongoose.connection.on('error', (err) => {
+    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+  });
   res.send(res.locals.valid);
 };
