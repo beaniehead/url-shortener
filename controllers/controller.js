f9e171a1e8c519a366800d2372cd059b16958cc1
Glitch (shorts) <none>
2018-01-20 21:42:17 +0000

    📌🏏 Checkpoint controllers/controller.js:576250/28

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,44 +15,47 @@ exports.urlvalidate = (req, res, next) => {
   }
 }
 exports.urlshorten = (req, res) => {
-//   const url = process.env.DATABASE;
-//   // Connect to DB
-//   MongoClient.connect(url, (err, db) => {
-//     assert.equal(null, err);
-//     if (err) throw err;
-//     console.log("Successfully connected to db");
-//     const dbo = db.db("url-shortener");
-//     // Check to see if entered url exists in database
-//     const urls = dbo.collection("urls");
-//     urls.find({
-//       oldUrl: {
-//         $eq: res.locals.urii
-//       }
-//     }).toArray((err, docs) => {
-//       if (err) throw err;
-//       //if the document does exist return the object to the user
-//       if (docs[0]) {
-//         // exists(docs[0]);
-//       } else {
-//         //disexists(res.locals.urii);
-//       }
-//       dbo.close();
-//     });
-//     //     function createHash(id) {
-//     //           const hashids = new Hashids();
-//     //           return hashids.encode(id);
-//     //         };
-//     //     function exists(doc){
-//     //     //return document to user
-//     //     console.log(`Document exists! See: ${JSON.stringify(doc)}`);
-//     //     };
-//     //     function disexists(uri){
-//     //     const base = "https://shorts.glitch.me/";  
-//     //     //create hash as suffix for new url
-//     //     const suffix = createHash(Date.now());
-//     //     const newUrl = `${base}${suffix}`;
-//     //         console.log(newUrl);
-//     //     }
-  // });
-  // res.send(res.locals.valid);
+  const url = process.env.DATABASE;
+  // Connect to DB
+  MongoClient.connect(url, (err, db) => {
+    assert.equal(null, err);
+    if (err) throw err;
+    console.log("Successfully connected to db");
+    const dbo = db.db("url-shortener");
+    // Check to see if entered url exists in database
+    const urls = dbo.collection("urls");
+
+    function createHash(id) {
+      const hashids = new Hashids();
+      return hashids.encode(id);
+    };
+    function exists(doc) {
+      //return document to user
+      console.log(`Document exists! See: ${JSON.stringify(doc)}`);
+    };
+    function disexists(uri) {
+      const base = "https://shorts.glitch.me/";
+      //create hash as suffix for new url
+      const suffix = createHash(Date.now());
+      const newUrl = `${base}${suffix}`;
+      console.log(newUrl);
+    }
+
+    urls.find({
+      oldUrl: {
+        $eq: res.locals.urii
+      }
+    }).toArray((err, docs) => {
+      if (err) throw err;
+      //if the document does exist return the object to the user
+      if (docs[0]) {
+        // exists(docs[0]);
+      } else {
+        //disexists(res.locals.urii);
+      }
+      dbo.close();
+    });
+
+  });
+  res.send(res.locals.valid);
 };
