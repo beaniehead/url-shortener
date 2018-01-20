42eef9a5a8c52b5c7a979071f23720d918a8e756
Glitch (shorts) <none>
2018-01-20 21:31:16 +0000

    🍺🛫 Checkpoint controllers/controller.js:576250/144

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,7 @@
 const validateURL = require("valid-url");
-const url = require("url");
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const URL = require("url");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 const Hashids = require("hashids");
 exports.urlvalidate = (req, res, next) => {
   const urii = req.params[0];
@@ -12,58 +12,47 @@ exports.urlvalidate = (req, res, next) => {
   } else {
     res.locals.urii = urii;
     next();
-  };
-};
+  }
+}
 exports.urlshorten = (req, res) => {
-  
-  const url = process.env.DATABASE;
-  // Connect to DB
-  MongoClient.connect(url, (err, db) => {
-    
-    assert.equal(null, err);
-    if (err) throw err;
-    console.log("Successfully connected to db");
-    const dbo = db.db("url-shortener");
-    // Check to see if entered url exists in database
-    const urls = dbo.collection("urls");
-    
-//     function createHash(id) {
-//           const hashids = new Hashids();
-//           return hashids.encode(id);
-//         };
-    
-//     function exists(doc){
-//     //return document to user
-//     console.log(`Document exists! See: ${JSON.stringify(doc)}`);
-//     };
-    
-//     function disexists(uri){
-//     const base = "https://shorts.glitch.me/";  
-//     //create hash as suffix for new url
-//     const suffix = createHash(Date.now());
-//     const newUrl = `${base}${suffix}`;
-//         console.log(newUrl);
-   
-//     }
-    
-    urls.find({
-      oldUrl: {
-        $eq: res.locals.urii
-      }
-    }).toArray((err, docs) => {
-      if (err) throw err;
-      //if the document does exist return the object to the user
-    
-      if(docs[0]){
-     // exists(docs[0]);
-      } else {
-      //disexists(res.locals.urii);
-      }
-      
-      //if the document doesn't exist - 
-    });
-  
-    
-  });
-  res.send(res.locals.valid);
+//   const url = process.env.DATABASE;
+//   // Connect to DB
+//   MongoClient.connect(url, (err, db) => {
+//     assert.equal(null, err);
+//     if (err) throw err;
+//     console.log("Successfully connected to db");
+//     const dbo = db.db("url-shortener");
+//     // Check to see if entered url exists in database
+//     const urls = dbo.collection("urls");
+//     urls.find({
+//       oldUrl: {
+//         $eq: res.locals.urii
+//       }
+//     }).toArray((err, docs) => {
+//       if (err) throw err;
+//       //if the document does exist return the object to the user
+//       if (docs[0]) {
+//         // exists(docs[0]);
+//       } else {
+//         //disexists(res.locals.urii);
+//       }
+//       dbo.close();
+//     });
+//     //     function createHash(id) {
+//     //           const hashids = new Hashids();
+//     //           return hashids.encode(id);
+//     //         };
+//     //     function exists(doc){
+//     //     //return document to user
+//     //     console.log(`Document exists! See: ${JSON.stringify(doc)}`);
+//     //     };
+//     //     function disexists(uri){
+//     //     const base = "https://shorts.glitch.me/";  
+//     //     //create hash as suffix for new url
+//     //     const suffix = createHash(Date.now());
+//     //     const newUrl = `${base}${suffix}`;
+//     //         console.log(newUrl);
+//     //     }
+  // });
+  // res.send(res.locals.valid);
 };
