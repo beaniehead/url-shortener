0986ecce4b2fb4c4a811f6a45780d9903cad19d2
Glitch (shorts) <none>
2018-01-20 02:28:05 +0000

    🚀🌁 Checkpoint controllers/controller.js:576250/443 package.json:576250/1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,13 @@
 const validateURL = require("valid-url");
+const url = require("url");
 exports.urlparse = (req, res) => {
   // const urii = req.params.url;
   const urii = "https://www.google.com";
   
-           if(validateURL.isHttpUri(urii)) {console.log( true)};
+  if (validateURL.isHttpsUri(urii)) {
+    return urii
+  } else {
+    return "SHIT!";
+  };
   res.send("Dave");
 }
